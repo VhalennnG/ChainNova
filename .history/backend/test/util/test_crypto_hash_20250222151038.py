e082ec7aa98ee3e5db645cd6a40460960b370bb5
VhalennnG <fanydanny237@gmail.com>
2025-05-25 19:49:53 +0800

    new

--- .history/backend/test/util/test_crypto_hash_20250222151038.py
+++
@@ -1,4 +0,0 @@
-from backend.util.crypto_hash import crypto_hash
-
-def test_crypto_hash():
-  assert crypto_hash(1, [2], 'three') == crypto_hash('three', [2], 1)
--- .history/backend/tests/util/test_crypto_hash_20250222152316.py
+++
@@ -1,7 +0,0 @@
-from backend.util.crypto_hash import crypto_hash
-
-def test_crypto_hash():
-  # It should create the same hash with arguments of different data types
-  # in any order
-  assert crypto_hash(1, [2], 'three') == crypto_hash('three', [2], 1)
-  assert crypto_hash('test cypto') == 'io'
